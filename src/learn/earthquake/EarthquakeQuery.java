2c9765391855a569d8843c342cb9eec9d6ff03e2
mirek <mlatka@gmail.com>
2018-07-25 09:52:32 +0200

    Initial commit

---
+++ src/learn/earthquake/EarthquakeQuery.java
@@ -0,0 +1,85 @@
+package learn.earthquake;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class EarthquakeQuery {
+
+
+    // https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02
+
+
+    public static String  findEarthquakes(int lastDays){
+
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDate endDate= currentTime.toLocalDate();
+        LocalDate startDate=endDate.minusDays(lastDays);
+
+        String searchResult="";
+
+        StringBuilder sb=generateURLBeginning(startDate,endDate);
+
+        return usgsQuery(sb).toString();
+    }
+
+
+    private  static StringBuilder generateURLBeginning(LocalDate startDate, LocalDate endDate){
+
+        StringBuilder sb = new StringBuilder("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson");
+        sb.append("&starttime=");
+        sb.append(startDate);
+        sb.append("&endtime=");
+        sb.append(endDate);
+
+        return sb;
+
+    }
+
+    private static StringBuffer usgsQuery(StringBuilder url){
+
+
+        StringBuffer response = new StringBuffer();
+
+        try {
+            URL obj = new URL(url.toString());
+            HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            in.close();
+
+        } catch (MalformedURLException ex) {
+
+            System.out.println("bad url");
+        } catch (IOException ex) {
+
+            System.out.println("Connection failed");
+
+        }
+
+
+        return response;
+
+
+
+
+    }
+
+
+
+}
---
+++ src/learn/earthquake/EarthquakeTester.java
@@ -0,0 +1,11 @@
+package learn.earthquake;
+
+public class EarthquakeTester {
+
+    public static void main(String[] args) {
+
+        System.out.println(EarthquakeQuery.findEarthquakes(1));
+
+
+    }
+}
